a47bd8ab611debb5dd110604afdb8f8c4f640b00
Muhammad mahdi <danyalm50@gmail.com>
2020-11-08 18:50:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,20 @@
 class Dog
-  def initialize(:name)
-    @name = name
+  def initialize(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
   end
 
-def name=(name)
-  @name = name
+def name=(dogs_name, dogs_breed = "Mutt")
+  @name = dogs_name
+  @breed = dogs_breed
 end
 
 def name
   @name
 end
+def breed
+  @breed
 end
 
-class Dog
-  def initialize(breed)
-    @breed = "Mutt"
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
 end
+
